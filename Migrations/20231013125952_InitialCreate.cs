3bcb3945d0deae32180d78c317a8eab93306042b
abdulazizbek711 <panjiyevabdulaziz77@gmail.com>
2023-10-24 17:14:16 +0500

    first commit

---
+++ Migrations/20231013125952_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MarketApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Product_ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Product_type = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    ProductQuantity_type = table.Column<int>(type: "integer", nullable: false),
+                    Price_Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Price_Currency = table.Column<string>(type: "text", nullable: false),
+                    ProductionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Discount = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Product_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    User_ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "integer", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.User_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Order_number = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Product_ID = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    User_ID = table.Column<int>(type: "integer", nullable: false),
+                    Price_Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Price_Currency = table.Column<string>(type: "text", nullable: false),
+                    User_ID1 = table.Column<int>(type: "integer", nullable: false),
+                    OrderQuantity_type = table.Column<int>(type: "integer", nullable: false),
+                    PaymentMethod = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Order_number);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_User_ID1",
+                        column: x => x.User_ID1,
+                        principalTable: "Users",
+                        principalColumn: "User_ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProduct",
+                columns: table => new
+                {
+                    OrdersOrder_number = table.Column<int>(type: "integer", nullable: false),
+                    ProductsProduct_ID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProduct", x => new { x.OrdersOrder_number, x.ProductsProduct_ID });
+                    table.ForeignKey(
+                        name: "FK_OrderProduct_Orders_OrdersOrder_number",
+                        column: x => x.OrdersOrder_number,
+                        principalTable: "Orders",
+                        principalColumn: "Order_number",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderProduct_Products_ProductsProduct_ID",
+                        column: x => x.ProductsProduct_ID,
+                        principalTable: "Products",
+                        principalColumn: "Product_ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProduct_ProductsProduct_ID",
+                table: "OrderProduct",
+                column: "ProductsProduct_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_User_ID1",
+                table: "Orders",
+                column: "User_ID1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProduct");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
